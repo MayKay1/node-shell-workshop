ac185fcfb290901556d33f374e9eb1942ae1de6e
Mary Anhalt <anhaltmk@gmail.com>
2022-06-15 16:58:18 -0400

    added updated bash.js

---
+++ bash.js
@@ -0,0 +1,12 @@
+//let { cwd } = require('node:process');
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
+    if (cmd === 'pwd') {
+        process.stdout.write(process.cwd());
+    }
+    
+    process.stdout.write('\nprompt > '); 
+});
+
